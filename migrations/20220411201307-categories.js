b1af0876eb812d04568997f004187c727b2247b2
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-11 18:03:41 -0300

    cria migration para tabela Categories

--- migrations/20220411201307-categories.js
+++ migrations/20220411201307-categories.js
@@ -2,20 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Categories');
   }
 };
