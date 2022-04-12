9cb8679bd786331a12594cf2a03f758343eb611b
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-12 10:41:45 -0300

    altera nome de coluna na Tabela users

--- migrations/20220411201251-users.js
+++ migrations/20220411201251-users.js
@@ -9,7 +9,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
       },
-      name: {
+      displayName: {
         type: Sequelize.STRING,
         allowNull: false,
       },
