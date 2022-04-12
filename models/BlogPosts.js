3c6ab5fd2ad78b6e56f91964d26a2fbc0bf15353
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-12 15:58:11 -0300

    adiciona model para BlogPosts

---
+++ models/BlogPosts.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPosts = sequelize.define('BlogPosts', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    tableName: 'BlogPosts',
+    timestamps: false,
+  });
+
+  BlogPosts.associate = (models) => {
+    BlogPosts.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'users' });
+  };
+
+  return BlogPosts;
+};
