0c20b03f894a10e42b43e40cf55bcc66e4fcbf50
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-28 14:20:19 -0300

    feat: adiciona rota get /user/:id

--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 // User
 router.post('/user', userController.create);
 router.get('/user', verifyAuth, userController.getAll);
+router.get('/user/:id', verifyAuth, userController.getById);
 
 // Login
 router.post('/login', loginController.login);
