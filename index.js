ffd89f2b5ddeeeee0cef595a2580be9cfbbff1ae
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-28 09:56:30 -0300

    feat: implementa rotas da api

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const router = require('./routes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use(errorMiddleware);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
