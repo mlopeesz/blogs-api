a99526f614fbfc81098cf36238ea83b5a2d03864
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-28 11:41:55 -0300

    feat: adiciona rota de login

--- routes.js
+++ routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userController = require('./controllers/userController');
+const loginController = require('./controllers/loginController');
 
 const router = express.Router();
 
 router.post('/user', userController.create);
+
+router.post('/login', loginController.login);
 
 module.exports = router;
