175112a2ec617b8a0ac329df07ccba96e0559089
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-28 16:23:36 -0300

    feat: rota post /categories

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('./controllers/userController');
 const loginController = require('./controllers/loginController');
+const categoriesController = require('./controllers/categoriesController');
 const verifyAuth = require('./middlewares/verifyAuth');
 
 const router = express.Router();
@@ -12,5 +13,8 @@ router.get('/user/:id', verifyAuth, userController.getById);
 
 // Login
 router.post('/login', loginController.login);
+
+// Categories
+router.post('/categories', verifyAuth, categoriesController.create);
 
 module.exports = router;
