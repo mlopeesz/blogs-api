cd092640c7216939347753e5acd49b39f9c53ba6
Mauricio Lopes <mlopeesz@gmail.com>
2022-04-28 15:42:07 -0300

    refact: mudança na string

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,7 +2,7 @@ const errorMiddleware = (err, _req, res, _next) => {
   if (err.status) {
     return res.status(err.status).json({ message: `${err.message}` });
   }
-  return res.status(500).json({ message: 'Servidor não encontrado' });
+  return res.status(500).json({ message: 'Server error' });
 };
 
 module.exports = errorMiddleware;
