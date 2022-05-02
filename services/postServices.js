0c47cd452f0b15a401bed25da15170f7b72975c1
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-02 11:30:04 -0300

    feat: função create / Services

---
+++ services/postServices.js
@@ -0,0 +1,36 @@
+const { Categories } = require('../models');
+const { BlogPosts } = require('../models');
+const validatePost = require('../validates/validatePost');
+const getError = require('../helpers/getError');
+
+const checkCategories = async (postData) => {
+  const { categoryIds } = postData;
+  const categories = await Categories.findAll();
+  const categoryExist = categoryIds
+    .every((category) => categories
+      .map(({ id }) => id)
+      .includes(category));
+
+  if (!categoryExist) {
+    throw getError(400, '"categoryIds" not found');
+  } 
+};
+
+const create = async (postData, userId) => {
+  const { error } = validatePost.validate(postData);
+
+  if (error) {
+    throw getError(400, error.message);
+  }
+
+  await checkCategories(postData);
+
+  const { title, content, categoryIds } = postData;
+  const createdPost = await BlogPosts.create({ title, content, userId });
+  createdPost.addCategory(categoryIds);
+  return createdPost;
+};
+
+module.exports = {
+  create,
+};
